9c86a46220498997206e5ac791073589a41998a9
Yalım Doğan <yalimdre@gmail.com>
2022-10-01 14:02:50 +0300

    Argument parser int types are defined

--- shadow_remover_test.py
+++ shadow_remover_test.py
@@ -9,13 +9,13 @@ if __name__ == "__main__":
     parser.add_argument('-i', '--image', required=True, help="Image of interest")
     parser.add_argument('-s', '--save', help= "Save the result",
                         default=True, nargs='?')
+    parser.add_argument('--ab', help="AB channel mean sum threshold", type=int, default=256)
     parser.add_argument('--lab', help="Adjust the pixel values according to LAB",
                         default=False, nargs='?')
-    parser.add_argument('--ab', help="AB channel mean sum threshold", default=256)
-    parser.add_argument('--rk', help="Region Adjustment Kernel Size", default=10)
-    parser.add_argument('--sdk', help="Shadow Dilation Kernel Size", default=3)
-    parser.add_argument('--sdi', help="Shadow Dilation Iteration", default=5)
-    parser.add_argument('--sst', help="Shadow size threshold", default=2500)
+    parser.add_argument('--rk', help="Region Adjustment Kernel Size", type=int, default=10)
+    parser.add_argument('--sdk', help="Shadow Dilation Kernel Size", type=int, default=3)
+    parser.add_argument('--sdi', help="Shadow Dilation Iteration", type=int, default=5)
+    parser.add_argument('--sst', help="Shadow size threshold", type=int, default=2500)
     parser.add_argument('-v', '--verbose', help="Verbose", const=True,
                         default=False, nargs='?')
     args = parser.parse_args()
